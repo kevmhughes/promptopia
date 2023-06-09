c163769a9f0268c7e03410e352fc43088adc3f42
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2023-06-09 10:40:45 +0200

    Add basic elements to page.jsx and create components file structure

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,18 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <spa className="orange_gradient text-center"> AI-Powered Prompts</spa>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is an open-source AI prompting tool for the modern world to
+        discover, create and share creative prompts.
+      </p>
+      {/* Feed */}
+    </section>
+  );
 };
 
 export default Home;
