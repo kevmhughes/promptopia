3bf3ba3c5afb2923a85afa26df549ddcc22fb7ed
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2023-06-10 03:21:26 +0200

    Create route.js and update layout.jsx and Provider.jsx

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,7 +1,15 @@
+"use client";
+
 import React from "react";
 
-const Provider = () => {
-  return <div>Provider</div>;
+import { SessionProvider } from "next-auth/react";
+
+const Provider = ({ children, session }) => {
+  return (
+    <div>
+      <SessionProvider session={session}>{children}</SessionProvider>
+    </div>
+  );
 };
 
 export default Provider;
