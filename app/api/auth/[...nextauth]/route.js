b119bb7b2dc8c92c5cc23c7b4bdbd11da6712324
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2023-06-14 00:25:28 +0200

    Update Nav.jsx, database.js and route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,11 +4,6 @@ import GoogleProvider from "next-auth/providers/google";
 import User from "@models/user";
 import { connectToDB } from "@utils/database";
 
-console.log({
-  clientId: process.env.GOOGLE_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-});
-
 const handler = NextAuth({
   providers: [
     GoogleProvider({
@@ -16,38 +11,34 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async session({ session }) {
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
 
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email,
-    });
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
 
-    session.user.id = sessionUser._id.toString();
+        // check if user already exists
+        const userExists = await User.findOne({ email: profile.email });
 
-    return session;
-  },
+        // if not, create a new document and save user in MongoDB
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
 
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
-
-      // check is a user already exists
-      const userExists = await User.findOne({
-        email: profile.email,
-      });
-
-      // if not, create a new user
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.image,
-        });
-      }
-
-      return true;
-    } catch (error) {}
-    return false;
+        return true;
+      } catch (error) {}
+      return false;
+    },
   },
 });
 
