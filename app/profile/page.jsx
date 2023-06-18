cd74e35a3bb364114b39714d33f021d8f106d04b
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2023-06-18 16:51:18 +0200

    Create prompt/[id]/route.js and posts/route/js and update page.jsx, Profile.jsx and PromptCard.jsx
    Add endpoints and GET, PATCH, DELETE methods to edit or delete prompts

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+"use client";
 
-const Profile = () => {
-  return <div>Profile</div>;
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Profile from "@components/Profile";
+
+const MyProfile = () => {
+  const { data: session } = useSession();
+  const [myPosts, setMyPosts] = useState([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+
+      setMyPosts(data);
+    };
+
+    if (session?.user.id) fetchPosts();
+  }, []);
+
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?/id=${post._id}`);
+  };
+
+  const handleDelete = async (post) => {};
+
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your personalised profile page"
+      data={myPosts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
 };
 
-export default Profile;
+export default MyProfile;
